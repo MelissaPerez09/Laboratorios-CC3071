267345f898ef4fa55b10604ff45d1dc6dd1fd1c9
Melissa Pérez <per21385@uvg.edu.gt>
2024-03-04 11:44:37 -0600

    fix: tokens parser

--- lexicalAnalyzer/yalexParser.py
+++ lexicalAnalyzer/yalexParser.py
@@ -7,79 +7,47 @@ class YALexParser:
     def __init__(self, file_path):
         self.file_path = file_path
         self.token_rules = []
+        self.definitions = {}
 
     def parse(self):
-        definitions = {}
         with open(self.file_path, 'r') as file:
-            for line in file:
+            lines = file.readlines()
+            in_rule = False
+            for line in lines:
                 line = line.split('(*')[0].strip()
                 if line.startswith('let'):
                     parts = line.split('=')
                     name = parts[0].strip().split()[1]
-                    value = parts[1].strip()
-                    ascii_value = self.replace_characters_with_ascii(value)
-                    definitions[name] = (value, ascii_value)
-                elif not line or line.startswith('rule tokens ='):
-                    continue
-                elif line.endswith('}'):
-                    action = line.split('{')[1].split('}')[0].strip()
-                    token_name = action.split()[1]
-                    self.token_rules.append((action, token_name))
-        self.definitions = definitions
+                    value = parts[1].strip().replace("'", "").replace(" ", "")
+                    self.definitions[name] = value
+                elif line.startswith('rule tokens'):
+                    in_rule = True
+                elif in_rule and '{' in line and line != '}':
+                    parts = line.strip().split('{')
+                    token_pattern = parts[0].strip()
+                    if '|' in token_pattern:
+                        token_pattern = token_pattern.split('|')[1].strip()
+                    token_name = parts[1].split('}')[0].strip().split()[1]
+                    self.token_rules.append((token_name, token_pattern))
+                elif line == '':
+                    in_rule = False
 
-    def replace_characters_with_ascii(self, value):
-        replaced_value = ""
-        in_quotes = False
-        for char in value:
-            if char == "'" and not in_quotes:
-                in_quotes = True
-                replaced_value += char
-            elif char == "'" and in_quotes:
-                in_quotes = False
-                replaced_value += char
-            elif in_quotes:
-                replaced_value += f" ({ord(char)})"
-            else:
-                replaced_value += char
-        return replaced_value
+    def replace_definitions(self, pattern):
+        prev_pattern = None
+        while prev_pattern != pattern:
+            prev_pattern = pattern
+            for def_name, def_value in self.definitions.items():
+                pattern = pattern.replace(def_name, def_value)
+        return pattern
 
-    def get_token_rules(self):
-        return self.token_rules
-
-    def get_definitions(self):
-        return self.definitions
-
-    def generate_id_regex(self):
-        token_rules = self.get_token_rules()
-        definitions = self.get_definitions()
-
-        id_definition = definitions.get('id', None)
-        number_definition = definitions.get('number', None)
-        
-        if id_definition:
-            id_regex = id_definition[0]
-        else:
-            id_regex = ""
-        
-        if number_definition:
-            number_regex = number_definition[0]
-        else:
-            number_regex = ""
-
-        for name, (original, _) in definitions.items():
-            id_regex = id_regex.replace(name, original)
-            number_regex = number_regex.replace(name, original)
-
-        for action, token_name in token_rules:
-            id_regex = id_regex.replace(token_name, action)
-            number_regex = number_regex.replace(token_name, action)
-
-        id_regex = id_regex.replace("'", "").replace('"', "")
-        number_regex = number_regex.replace("'", "").replace('"', "")
-
-        return id_regex, number_regex
+    def generate_all_regex(self):
+        regexes = {}
+        for token_name, token_pattern in self.token_rules:
+            regex = self.replace_definitions(token_pattern)
+            regex = regex.replace('s', '+')
+            regexes[token_name] = regex
+        return regexes
 
 parser = YALexParser('./yalex/slr-4.yal')
 parser.parse()
-print(parser.generate_id_regex())
-print(parser.get_token_rules())
+print(parser.generate_all_regex())
