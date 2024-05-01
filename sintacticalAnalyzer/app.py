a22b9e88451f9dedaf786fcd6e88d200de54a75b
Melissa Pérez <per21385@uvg.edu.gt>
2024-05-01 00:21:02 -0600

    UI implemented

---
+++ sintacticalAnalyzer/app.py
@@ -0,0 +1,115 @@
+"""
+app.py
+Sintactial Analyzer App
+"""
+
+import sys
+sys.path.insert(0, '/Users/melissa/Desktop/UVG/lenguajes/CC3071-LabAB/')
+
+from lexicalAnalyzer.yalexParser import *
+from sintacticalAnalyzer.gammarUI import *
+
+import tkinter as tk
+from tkinter import filedialog, Text
+
+root = tk.Tk()
+root.title("Sintactical Analyzer Generator")
+
+def open_file(editor, filetypes):
+    filepath = filedialog.askopenfilename(filetypes=filetypes)
+    if filepath:
+        with open(filepath, 'r') as file:
+            content = file.read()
+            editor.delete('1.0', tk.END)
+            editor.insert(tk.END, content)
+        editor.filepath = filepath
+
+def save_as_file(editor, default_extension):
+    filetypes = [(f"{default_extension.upper()} Files", f"*.{default_extension}")]
+    filepath = filedialog.asksaveasfilename(filetypes=filetypes, defaultextension=f".{default_extension}")
+    if filepath:
+        content = editor.get('1.0', tk.END)
+        with open(filepath, 'w') as file:
+            file.write(content)
+        editor.filepath = filepath
+
+def print_to_console(msg):
+    console.insert(tk.END, msg + '\n')
+    console.see(tk.END)
+
+old_print = print
+def print(*args, **kwargs):
+    old_print(*args, **kwargs)
+    msg = ' '.join(map(str, args))
+    print_to_console(msg)
+
+def generate_analysis():
+    # Uso de los parsers
+    if hasattr(editor_yalex, 'filepath') and editor_yalex.filepath and hasattr(editor_yapar, 'filepath') and editor_yapar.filepath:
+        yapar_parser = YAParParser(editor_yapar.filepath)
+        yapar_parser.parse()
+        yapar_parser.print_grammar()
+
+        yalex_parser = YALexParser(editor_yalex.filepath)
+        yalex_parser.parse()
+        yalex_tokens = yalex_parser.generate_all_regex()
+
+        # Validación de tokens y generación de autómatas
+        is_valid, missing_tokens = validate_tokens(yapar_parser.tokens, yalex_tokens)
+        if not is_valid:
+            print(f"Token validation: False\nTokens missing: {missing_tokens}")
+        else:
+            print("Token validation: True")
+
+            automata = AutomataLR0(yapar_parser.grammar, yapar_parser.tokens)
+            automata.build_states()
+            automata.parsing_actions()
+
+            generate_automata_graph(automata, 'automataLR(0)')
+
+            print("Automata and parser generated successfully!")
+    else:
+        print("Please load both YALex and YAPar files before generating analysis.")
+
+# Frames para los editores de texto
+frame_yalex = tk.Frame(root)
+frame_yalex.grid(row=1, column=0, padx=10, pady=10)
+
+frame_yapar = tk.Frame(root)
+frame_yapar.grid(row=1, column=1, padx=10, pady=10)
+
+# Editores de texto
+editor_yalex = Text(frame_yalex, height=20, width=40)
+editor_yalex.pack()
+editor_yalex.filepath = None
+
+editor_yapar = Text(frame_yapar, height=20, width=40)
+editor_yapar.pack()
+editor_yapar.filepath = None
+
+# Botones para abrir los archivos
+button_open_yalex = tk.Button(root, text="Open YALex", command=lambda: open_file(editor_yalex, [('YALex Files', '*.yal')]))
+button_open_yalex.grid(row=0, column=0, padx=10, pady=10)
+
+button_open_yapar = tk.Button(root, text="Open YAPar", command=lambda: open_file(editor_yapar, [('YAPar Files', '*.yalp')]))
+button_open_yapar.grid(row=0, column=1, padx=10, pady=10)
+
+# Botones para guardar los archivos
+button_saveas_yalex = tk.Button(frame_yalex, text="Save As", command=lambda: save_as_file(editor_yalex, 'yal'))
+button_saveas_yalex.pack()
+
+button_saveas_yapar = tk.Button(frame_yapar, text="Save", command=lambda: save_as_file(editor_yapar, 'yalp'))
+button_saveas_yapar.pack()
+
+# Botón para generar el analizador
+button_generate = tk.Button(root, text="Generate", command=generate_analysis, width=20)
+button_generate.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+# Consola de salida
+console_frame = tk.Frame(root)
+console_frame.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+console = Text(console_frame, height=10, width=80)
+console.pack()
+
+root.mainloop()
---
+++ sintacticalAnalyzer/gammarUI.py
@@ -0,0 +1,183 @@
+"""
+gammarUI.py
+Define la estructura de la gramática utilizada por el analizador sintáctico
+Se utiliza en la interfaz gráfica
+"""
+
+import sys
+import graphviz
+sys.path.insert(0, '/Users/melissa/Desktop/UVG/lenguajes/CC3071-LabAB/')
+
+from lexicalAnalyzer.yalexParser import YALexParser
+
+class YAParParser:
+    def __init__(self, yapar_path):
+        self.yapar_path = yapar_path
+    
+    def parse(self):
+        grammar = {}
+        tokens = set()
+        
+        with open(self.yapar_path, 'r') as file:
+            content = file.readlines()
+        
+        current_section = None
+        current_head = None
+        for line in content:
+            line = line.strip()
+            if line.startswith('/*') and line.endswith('*/'):
+                continue
+            elif line.startswith('%token'):
+                parts = line.split()
+                tokens.update(parts[1:])
+            elif line.startswith('%%'):
+                current_section = 'rules'
+            elif current_section == 'rules' and line:
+                if line.endswith(';'):
+                    line = line[:-1]
+                if ':' in line:
+                    head, production = line.split(':')
+                    head = head.strip()
+                    current_head = head
+                    grammar[current_head] = []
+                    line = production
+                if current_head:
+                    productions = [prod.strip() for prod in line.split('|')]
+                    for prod in productions:
+                        if prod:
+                            production = tuple(prod.split())
+                            grammar[current_head].append(production)
+            elif line.startswith('IGNORE'):
+                _, ignore_token = line.split()
+                if ignore_token in tokens:
+                    tokens.remove(ignore_token)
+                    
+        self.grammar = grammar
+        self.tokens = tokens
+
+    def print_grammar(self):
+        print("\n----------------------------\nDetected grammar:\n----------------------------")
+        for nonterminal, productions in self.grammar.items():
+            print(f"{nonterminal} -> {[' '.join(prod) for prod in productions]}")
+
+class AutomataLR0:
+    def __init__(self, grammar, tokens):
+        self.grammar = grammar
+        self.tokens = tokens
+        self.start_symbol = None
+        self.states = []
+        self.transitions = {}
+        self.actions = {}
+        self.augment_grammar()
+
+    def augment_grammar(self):
+        self.start_symbol = 'S\''
+        original_start_symbol = next(iter(self.grammar))
+        self.grammar[self.start_symbol] = [(original_start_symbol, '$')]
+
+    def closure(self, items):
+        closure_set = set(items)
+        while True:
+            new_items = set()
+            for (head, body, dot_position) in closure_set:
+                if dot_position < len(body):
+                    symbol = body[dot_position]
+                    if symbol in self.grammar:
+                        for prod in self.grammar[symbol]:
+                            new_item = (symbol, prod, 0)
+                            if new_item not in closure_set:
+                                new_items.add(new_item)
+            if not new_items:
+                break
+            closure_set |= new_items
+        return closure_set
+
+    def goto(self, state, symbol):
+        new_state = set()
+        for (head, body, dot_position) in state:
+            if dot_position < len(body) and body[dot_position] == symbol:
+                new_state.add((head, body, dot_position + 1))
+        return self.closure(new_state)
+
+    def build_states(self):
+        initial_item = (self.start_symbol, self.grammar[self.start_symbol][0], 0)
+        initial_state = self.closure({initial_item})
+        self.states = [initial_state]
+        transitions = {}
+
+        while True:
+            new_states = False
+            for state in list(self.states):
+                for symbol in self.grammar.keys() | self.tokens | {'$'}:
+                    next_state = self.goto(state, symbol)
+                    if next_state and next_state not in self.states:
+                        self.states.append(next_state)
+                        new_states = True
+                    if next_state:
+                        transitions[(tuple(state), symbol)] = next_state
+            if not new_states:
+                break
+
+        self.transitions = {k: self.states.index(v) for k, v in transitions.items()}
+
+    def parsing_actions(self):
+        for state_index, state in enumerate(self.states):
+            for item in state:
+                head, body, dot_position = item
+                if dot_position == len(body):  # We are at the end of a production.
+                    if head == self.start_symbol:  # Accept condition.
+                        self.actions[(state_index, '$')] = ('accept', None)
+                    else:
+                        for prod_index, prod in enumerate(self.grammar[head]):
+                            if prod == body:
+                                self.actions[(state_index, symbol)] = ('reduce', prod_index)
+                else:
+                    symbol = body[dot_position]
+                    if symbol in self.tokens:  # Shift condition.
+                        next_state = self.transitions[(tuple(state), symbol)]
+                        self.actions[(state_index, symbol)] = ('shift', next_state)
+
+            
+def extract_token_names(yalex_tokens):
+    token_names = set()
+    for token in yalex_tokens.keys():
+        if token.strip():
+            # Extract token name from print or return statement
+            start_quote = token.find("'") if token.find("'") != -1 else token.find('"')
+            end_quote = token.rfind("'") if token.rfind("'") != -1 else token.rfind('"')
+            if start_quote != -1 and end_quote != -1 and start_quote != end_quote:
+                token_name = token[start_quote + 1:end_quote]
+                token_names.add(token_name)
+            else:
+                print(f"Warning: Token '{token}' does not contain a valid token name")
+    return token_names
+
+def validate_tokens(yapar_tokens, yalex_tokens):
+    yalex_token_names = extract_token_names(yalex_tokens)
+    missing_tokens = yapar_tokens - yalex_token_names
+    if missing_tokens:
+        return False, missing_tokens
+    else:
+        return True, None
+
+def generate_automata_graph(automata, filename):
+    dot = graphviz.Digraph(format='png')
+
+    for i, state in enumerate(automata.states):
+        label = ""
+        for (head, body, dot_position) in state:
+            if head == "S'" and dot_position == len(body):
+                label = "ACCEPT"
+                break
+            else:
+                body_with_dot = body[:dot_position] + ('•',) + body[dot_position:]
+                label += f"  {head} -> {' '.join(body_with_dot)}\n"
+        dot.node(str(i), label if label != "ACCEPT" else "ACCEPT", shape='box')
+
+    for (state, symbol), next_state in sorted(automata.transitions.items()):
+        dot.edge(str(i), str(next_state), label=symbol)
+
+    dot.node("start", "", shape="none", width="0", height="0")
+    dot.edge("start", "0", shape="none")
+
+    dot.render(filename)
